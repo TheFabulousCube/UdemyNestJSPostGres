8d414b5c624b11fd9f3f2c044ab96f769d32c1d4
Marquardt Snell <thefabulouscube@tadaatiedye.com>
2023-09-21 22:55:23 -0400

    Unti tests for Tag Controller & Service

---
+++ mediumclone_nestjs/src/tag/tag.service.spec.ts
@@ -0,0 +1,46 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TagService } from './tag.service';
+import { TagEntity } from './tag.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+const mockTags = ['dragons', 'coffee', 'birds', 'plants'];
+const mockTagEntities = mockTags.map((tag, index) => ({
+  id: index,
+  name: tag,
+}));
+
+fdescribe('TagService', () => {
+  let service: TagService;
+  let tagRepository: Repository<TagEntity>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        TagService,
+        {
+          provide: getRepositoryToken(TagEntity),
+          useValue: {
+            find: jest.fn(() => {
+              return Promise.resolve(mockTagEntities);
+            }),
+            save: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+    service = module.get<TagService>(TagService);
+    tagRepository = module.get<Repository<TagEntity>>(
+      getRepositoryToken(TagEntity),
+    );
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should return all tags', async () => {
+    console.log('result: ', await service.findAll());
+    expect(service.findAll()).resolves.toBe(mockTagEntities);
+  });
+});
--- mediumclone_nestjs/src/tag/tag.service.ts
+++ mediumclone_nestjs/src/tag/tag.service.ts
@@ -8,22 +8,18 @@ export class TagService {
   constructor(
     @InjectRepository(TagEntity)
     private readonly tagRepository: Repository<TagEntity>,
-  ) { }
+  ) {}
 
   async findAll(): Promise<TagEntity[]> {
     return await this.tagRepository.find();
   }
 
   async saveAll(list: string[]): Promise<TagEntity[]> {
-    // const tags: TagEntity[] = list.map(item =>name: item);
-    const tags: TagEntity[]  = list.map((item) => {
-          const tag = new TagEntity();
-          tag.name = item;
-          return tag;
-        });
+    const tags: TagEntity[] = list.map((item) => {
+      const tag = new TagEntity();
+      tag.name = item;
+      return tag;
+    });
     return await this.tagRepository.save(tags);
-
   }
 }
-
-
